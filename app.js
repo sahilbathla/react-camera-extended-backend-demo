01ec08d5ec0f5697331cd72e5c7d192ffde428de
Sahil Batla <sahilbathla1@gmail.com>
2020-01-06 18:51:53 +0530

    Increase limit to support large payload for request and response

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = express();
 
 // view engine setup
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
