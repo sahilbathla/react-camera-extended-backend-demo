067de997fbbaa67a8c00bbf7d3d15288c15710fe
Sahil Batla <sahilbathla1@gmail.com>
2020-01-06 19:18:56 +0530

    Add support for base64 images as inline images to mail

--- src/controllers/ImagesController.js
+++ src/controllers/ImagesController.js
@@ -10,7 +10,15 @@ class ImagesController {
     await sendMail({
       subject: 'Your Uploaded Photo is Here!',
       to: (body.to || 'sahilbathla1@gmail.com'),
-      html: `There, you go enjoy your photo!<br><img src="${body.image}" />`,
+      html: `There, you go enjoy your photo!<br><img src="cid:image" />`,
+      attachments: [
+        {   // encoded string as an attachment
+          filename: 'image.png',
+          content: body.image.split("base64,")[1],
+          encoding: 'base64',
+          cid: 'image'
+        }
+      ]
     });
     res.json({message: CONSTANT.MESSAGES.IMAGES.SUCCESSFULLY_EMAILED });
   }
